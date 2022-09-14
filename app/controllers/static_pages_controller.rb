27d0201fabeb29bb43a9c2b66f494cdfd7e03b34
Alyoshka <alyoshkamyshko@gmail.com>
2022-09-13 20:18:41 -0700

    Set up flickr API to display public images of provide user id

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
+    require 'flickr'
+
     def index
+        flickr = Flickr.new ENV["api_key"], ENV["api_secret"]
+
+        begin
+            @photos = flickr.people.getPublicPhotos(user_id: params[:user_id])
+        rescue
+            @photos = []
+        end
     end
 end
